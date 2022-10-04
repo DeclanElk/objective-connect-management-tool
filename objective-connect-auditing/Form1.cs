4599a640c3f1c06c14e5bc93f763e180117e130b
DeclanElk <delkerton2001@gmail.com>
2022-10-04 19:19:39 +1000

    Setting up login form

--- objective-connect-auditing/Form1.cs
+++ objective-connect-auditing/Form1.cs
@@ -6,5 +6,30 @@ namespace objective_connect_auditing
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
